12a8722c4cd872ea0407075fe755b799f51862f2
Thales Spanhol <thales_spanhol@hotmail.com>
2023-07-03 19:32:49 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useRef } from "react";
+import { WasmBoy } from "wasmboy";
+import MK from "./assets/MK.gb";
+import "./App.css";
+
+const App = () => {
+  const canvasRef = useRef();
+
+  const setup = async () => {
+    const wasmBoyOptions = {
+      headless: false,
+      useGbcWhenOptional: true,
+      isAudioEnabled: true,
+      frameSkip: 1,
+      audioBatchProcessing: true,
+      timersBatchProcessing: false,
+      audioAccumulateSamples: true,
+      graphicsBatchProcessing: false,
+      graphicsDisableScanlineRendering: false,
+      tileRendering: true,
+      tileCaching: true,
+      gameboyFPSCap: 30,
+      updateGraphicsCallback: false,
+      updateAudioCallback: false,
+      saveStateCallback: false,
+    };
+    await WasmBoy.config(wasmBoyOptions, canvasRef.current);
+    await WasmBoy.loadROM(MK);
+    await WasmBoy.play();
+  };
+
+  return (
+    <>
+      <h1>MK - Gameboy</h1>
+      <button onClick={setup}>Play</button>
+      <canvas ref={canvasRef} className="game"></canvas>
+    </>
+  );
+};
+
+export default App;
